37b8c1c586299f388f41f5c306a8760f8d62286b
vian013 <kevin.nguyen0155@gmail.com>
2024-05-12 17:01:31 +0700

    Init commit

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const port = 8080;
+
+app.listen(port, () => console.log(`Server is listening on port ${port}...`));
+
+app.get("/", (req, res) => {
+  res.send({ message: "Hello World!" });
+});
+
+app.post("/", (req, res) => {
+  console.log(req.query);
+
+  res.send({ message: "Post method" });
+});
+
+const router = express.Router();
+app.use("/api", router);
+
+router.get("/users", (req, res) => {
+  res.send({ message: "Get users" });
+});
