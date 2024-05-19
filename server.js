c964e49c083d4ecd8d0f0dd64e0f025dc0f54472
vian013 <kevin.nguyen0155@gmail.com>
2024-05-19 11:45:29 +0700

    Update some text

--- server.js
+++ server.js
@@ -9,12 +9,11 @@ const port = 8080;
 app.listen(port, () => console.log(`Server is listening on port ${port}...`));
 
 app.get("/", (req, res) => {
-  res.send({ message: "Hello World!" });
+  res.send({ message: "Hello World 123!" });
 });
 
 app.post("/", (req, res) => {
   console.log(req.query);
-
   res.send({ message: "Post method" });
 });
 
